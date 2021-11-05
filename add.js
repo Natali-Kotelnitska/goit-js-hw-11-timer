e975a12921b8470897867df1a867bffe178d005f
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2021-11-05 16:50:01 +0200

    Update code

--- add.js
+++ add.js
@@ -1,40 +1,62 @@
-// Создай плагин настраиваемого таймера,
-//     который ведет обратный отсчет до предварительно определенной даты.
-//     Такой плагин может использоваться в блогах и интернет - магазинах,
-//     страницах регистрации событий, во время технического обслуживания и т.д.
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.intervalID = null;
 
-// Плагин ожидает следующую HTML - разметку и показывает четыре цифры:
-// дни, часы, минуты и секунды в формате XX: XX: XX: XX.
-// Количество дней может состоять из более чем двух цифр.
+    this.days = document.querySelector('[data-value="days"]');
+    this.hours = document.querySelector('[data-value="hours"]');
+    this.mins = document.querySelector('[data-value="mins"]');
+    this.secs = document.querySelector('[data-value="secs"]');
 
-// Плагин это класс CountdownTimer, экземпляр которого создает новый таймер с настройками.
+    this.start();
+    this.stop();
+  }
 
-new CountdownTimer({
-  selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2019"),
+  start() {
+    this.intervalID = setInterval(() => {
+      const currentTime = Date.now();
+      const timeDistance = this.targetDate - currentTime;
+      const time = this.getTimeComponents(timeDistance);
+      this.updateClockface(time);
+    }, 1000);
+  }
+
+  stop() {
+    if (this.timeDistance < 0) {
+      clearInterval(this.intervalID);
+      const time = this.getTimeComponents(0);
+      console.log(time);
+    }
+  }
+  // onClear = () => {
+  //   this.stop();
+  //   this.timeDistance = 0;
+  //   this.currentTime = 0;
+  //   this.getTimeComponents(this.timeDistance, this.selector);
+  // };
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  updateClockface({ days, hours, mins, secs }) {
+    this.days.textContent = `${days}`;
+    this.hours.textContent = `${hours}`;
+    this.mins.textContent = `${mins}`;
+    this.secs.textContent = `${secs}`;
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Nov 5, 2021'),
 });
-
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
